f51ee9b40f88c0f0fcd9a543ab31c2e3b063fcd9
odanchen <91546290+odanchen@users.noreply.github.com>
2021-10-05 22:41:42 +0300

    added task09

---
+++ contest3/task09.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int result= 0, counter = 0, mask = 1;
+    char num;
+    bool is_negative = false;
+    do {
+        cin >> num;
+        if (counter == 0 && num == '1') is_negative = true;
+        counter++;
+        result <<= 1;
+        if (num == '1') result += 1;
+    } while (cin.peek() != '\n');
+    if (is_negative) {
+        result = ~result;
+        result = result & (~(~0 << counter));
+        result = result + 1;
+        result = -result;
+    }
+    cout << result;
+}
