dd66dd6f4301eefaa55582b940f773b9fb71abd9
odanchen <91546290+odanchen@users.noreply.github.com>
2021-12-19 22:37:11 +0200

    Create visiting_friends.cpp

---
+++ eolymp_contest/visiting_friends.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace::std;
+
+int nnmain()
+{
+    int cor[3], len = 3;
+    cin >> cor[0] >> cor[1] >> cor[2];
+    sort(cor, cor + len);
+    if (cor[0] >= 0)
+        cout << cor[2] * 2;
+    else if (cor[2] <= 0)
+        cout << abs(cor[0]) * 2;
+    else
+        cout << abs(cor[0] - cor[2]) * 2;
+    return 0;
+}
