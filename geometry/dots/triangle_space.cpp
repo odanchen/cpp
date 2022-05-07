ceac1d23d44d675d5a84d31d873fac3ed0f16c11
odanchen <91546290+odanchen@users.noreply.github.com>
2022-05-07 10:48:48 +0200

    Create triangle_space.cpp

---
+++ geometry/dots/triangle_space.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+
+using namespace::std;
+
+struct cord
+{
+    double x;
+    double y;
+};
+
+double sqr(double num)
+{
+    return num * num;
+}
+
+double distance(cord dot1, cord dot2)
+{
+    return sqrt(sqr(dot1.x - dot2.x) + sqr(dot1.y - dot2.y));
+}
+
+void read_arr(cord arr[], int &len)
+{
+    cin >> len;
+    
+    for (int i = 0; i < len; i++)
+        cin >> arr[i].x >> arr[i].y;
+}
+
+int main()
+{
+    cord side1, side2, side3;
+    cin >> side1.x >> side1.y >> side2.x >> side2.y >> side3.x >> side3.y;
+    double p = (distance(side1, side2) + distance(side1, side3) + distance(side2, side3))/2;
+    double ans = sqrt(p * (p - distance(side1, side2)) * (p - distance(side1, side3))
+                      * (p - distance(side2, side3)));
+    cout << fixed << setprecision(3) << ans;
+}
