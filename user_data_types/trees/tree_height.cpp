451053e0df434c7778da6ca68f29413cb7c05493
odanchen <91546290+odanchen@users.noreply.github.com>
2022-10-17 16:52:17 -0400

    Create tree_height.cpp

---
+++ user_data_types/trees/tree_height.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+
+using namespace::std;
+
+struct tree
+{
+    tree *left = NULL;
+    tree *right = NULL;
+    int value = 0;
+};
+
+void traverse(tree *node, int curHeight, int &maxHeigth)
+{
+    if (node == NULL) return;
+    
+    traverse(node->left, curHeight + 1, maxHeigth);
+    maxHeigth = max(curHeight, maxHeigth);
+    traverse(node->right, curHeight + 1, maxHeigth);
+}
+
+int findHeight(tree *node)
+{
+    if (node == NULL) return 0;
+    
+    int maxHeight = 1, curHeight = 1;
+    traverse(node, curHeight, maxHeight);
+    return maxHeight;
+}
+
+void buildTree(tree *&root)
+{
+    int curValue = -1;
+    tree *curNode = root;
+    tree *prevNode = root;
+    cin >> curValue;
+    root->value = curValue;
+    
+    while(curValue != 0)
+    {
+        cin >> curValue;
+        curNode = root;
+        
+        if (curValue != 0)
+        {
+            while(curNode != NULL && curNode->value != curValue)
+            {
+                if (curValue > curNode->value)
+                {
+                    prevNode = curNode;
+                    curNode = curNode->right;
+                }
+                else
+                {
+                    prevNode = curNode;
+                    curNode = curNode->left;
+                }
+            }
+        
+            if (curNode == NULL)
+            {
+                if (curValue > prevNode->value)
+                {
+                    tree *addNode = new tree;
+                    addNode->value = curValue;
+                    prevNode->right = addNode;
+                }
+                else if (curValue < prevNode->value)
+                {
+                    tree *addNode = new tree;
+                    addNode->value = curValue;
+                    prevNode->left = addNode;
+                }
+            }
+        }
+    }
+}
+
+
+
+int main()
+{
+    tree *root = new tree;
+    buildTree(root);
+    cout << findHeight(root);
+}
