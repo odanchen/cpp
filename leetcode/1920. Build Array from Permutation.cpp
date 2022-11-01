6cc09a7325f9298d208d0d2d8ca7983e2cc320f6
odanchen <91546290+odanchen@users.noreply.github.com>
2022-11-01 16:56:02 -0400

    Create 1920. Build Array from Permutation.cpp

---
+++ leetcode/1920. Build Array from Permutation.cpp
@@ -0,0 +1,11 @@
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int> ans;
+        ans.resize(nums.size());
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
