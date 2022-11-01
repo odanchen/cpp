edc48d00458b9b343d8f488130375dad2880a6e3
odanchen <91546290+odanchen@users.noreply.github.com>
2022-11-01 16:51:44 -0400

    Create 27. Remove Element.cpp

---
+++ leetcode/27. Remove Element.cpp
@@ -0,0 +1,21 @@
+    int removeElement(vector<int>& nums, int val) {
+        int cnt = 0;
+        queue<int> idxs;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != val)
+            {
+                cnt++;
+                if (not(idxs.empty()))
+                {
+                    swap(nums[i], nums[idxs.front()]);
+                    idxs.pop();
+                    i--;
+                }
+            }
+            else idxs.push(i);
+        }
+
+
+        return cnt;
+    }
