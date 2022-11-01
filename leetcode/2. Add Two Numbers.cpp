8ac98406deccc54845075a289b6553a7af6a65a8
odanchen <91546290+odanchen@users.noreply.github.com>
2022-11-01 16:46:30 -0400

    Create 2. Add Two Numbers.cpp

---
+++ leetcode/2. Add Two Numbers.cpp
@@ -0,0 +1,39 @@
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *head = new ListNode();
+        ListNode *cur = head;
+        int temp = 0;
+        while (l1 != NULL || l2 != NULL)
+        {
+            int val1, val2;
+
+            if(l1 == NULL) val1 = 0;
+            else val1 = l1->val;
+
+            if(l2 == NULL) val2 = 0;
+            else val2 = l2->val;
+
+            int val3 = val1 + val2 + temp;
+
+            cur->val = (val3) % 10;
+            temp = (val3) / 10;
+
+            if (l1 != NULL) l1 = l1->next;
+            if (l2 != NULL) l2 = l2->next;
+
+            if (l1 != NULL || l2 != NULL)
+            {
+                ListNode *ptrNext = new ListNode();
+                cur->next = ptrNext;
+                cur = cur->next;
+            }
+            else if (temp == 1)
+            {
+                ListNode *ptrNext = new ListNode();
+                cur->next = ptrNext;
+                cur = cur->next;
+                cur->val = temp;
+            }
+        }
+
+        return head;
+    }
