9b9c4f81c75bbb782264d8d255c0d348d8e74196
odanchen <91546290+odanchen@users.noreply.github.com>
2022-11-01 16:45:55 -0400

    Create 1. Two Sum.cpp

---
+++ leetcode/1. Two Sum.cpp
@@ -0,0 +1,17 @@
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> hashMap;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (hashMap.find(nums[i]) != hashMap.end())
+            {
+                vector<int> ans = {hashMap[nums[i]], i};
+                return ans;
+            }
+            else
+            {
+                hashMap.insert({target - nums[i], i});
+            }
+        }
+
+        return nums;
+    }
