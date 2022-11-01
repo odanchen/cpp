a8f424f5bb99c11e1439b181779365986ae26690
odanchen <91546290+odanchen@users.noreply.github.com>
2022-11-01 16:47:19 -0400

    Create 3. Longest Substring Without Repeating Characters.cpp

---
+++ leetcode/3. Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,20 @@
+   int lengthOfLongestSubstring(string s) {
+        int i = 0, maxLen = 0, curLen = 0;
+        while(i != s.size())
+        {
+            int hash[128][2] = {0};
+            while(i != s.size() && hash[s[i]][0] == 0)
+            {
+                hash[s[i]][0] = 1;
+                hash[s[i]][1] = i;
+                i++;
+                curLen++;
+            }
+
+            maxLen = max(maxLen, curLen);
+            curLen = 0;
+            if(i != s.size()) i = hash[s[i]][1] + 1;
+        }
+
+        return maxLen;
+    }
