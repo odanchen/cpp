06564011a55bd604ff13b7e33ab51e0b9b89be3a
odanchen <91546290+odanchen@users.noreply.github.com>
2022-11-01 16:56:30 -0400

    Create 2011. Final Value of Variable After Performing Operations.cpp

---
+++ leetcode/2011. Final Value of Variable After Performing Operations.cpp
@@ -0,0 +1,10 @@
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
+        for (int i = 0; i < operations.size(); i++)
+        {
+            if(operations[i][0] == '+' || operations[i][2] == '+') x++;
+            else x--;
+        }
+
+        return x;
+    }
