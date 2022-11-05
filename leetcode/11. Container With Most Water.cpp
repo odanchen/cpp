7f65c84fe09e62e05d9f75e3c6dc05087a2c35e2
odanchen <91546290+odanchen@users.noreply.github.com>
2022-11-05 11:38:48 -0400

    Create 11. Container With Most Water.cpp

---
+++ leetcode/11. Container With Most Water.cpp
@@ -0,0 +1,16 @@
+    int getArea(int idx1, int idx2, vector<int> &height)
+    {
+        return (idx2 - idx1) * min(height[idx1], height[idx2]);
+    }
+    int maxArea(vector<int>& height) {
+        int ans = 0;
+        int left = 0, right = height.size() - 1;
+        while(left < right)
+        {
+            ans  = max(ans, getArea(left, right, height));
+            if(height[left] < height[right]) left++;
+            else right--;
+        }
+
+        return ans;
+    }
