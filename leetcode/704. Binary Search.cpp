effb4e9da1b4d39c9464b42d87a08ab45f02c635
odanchen <91546290+odanchen@users.noreply.github.com>
2022-11-09 06:40:07 -0500

    Create 704. Binary Search.cpp

---
+++ leetcode/704. Binary Search.cpp
@@ -0,0 +1,16 @@
+    int search(vector<int>& nums, int target) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        int left = 0, right = nums.size() - 1;
+        while(left < right)
+        {
+            int mid = (left + right) / 2;
+            if(nums[mid] == target) return mid;
+            if (nums[mid] < target) left = mid + 1;
+            else right = mid;
+        }
+
+        if (nums[left] == target) return left;
+        return - 1;
+    }
