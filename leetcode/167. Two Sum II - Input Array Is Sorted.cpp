4d210c7b6cf88f34b79adf8cf51f2024224b547e
odanchen <91546290+odanchen@users.noreply.github.com>
2022-11-11 06:35:09 -0500

    Create 167. Two Sum II - Input Array Is Sorted.cpp

---
+++ leetcode/167. Two Sum II - Input Array Is Sorted.cpp
@@ -0,0 +1,13 @@
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left, right;
+        left = 0;
+        right = numbers.size() - 1;
+
+        while(numbers[left] + numbers[right] != target)
+        {
+            if(numbers[left] + numbers[right] > target) right--;
+            else left++;
+        }
+
+        return (vector<int>) {left + 1, right + 1};
+    }
