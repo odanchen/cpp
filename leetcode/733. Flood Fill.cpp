80f5fede6d05633a458386a91bbabf614f420af4
odanchen <91546290+odanchen@users.noreply.github.com>
2022-11-15 03:01:07 -0500

    Create 733. Flood Fill.cpp

---
+++ leetcode/733. Flood Fill.cpp
@@ -0,0 +1,18 @@
+    void fill(int row, int col, int color, vector<vector<int>>& image, int startColor)
+    {
+        if (image[row][col] == startColor) image[row][col] = color;
+
+        if(col + 1 < image[row].size()  && image[row][col + 1] == startColor)
+            fill(row, col + 1, color, image, startColor);
+        if(col - 1 >= 0  && image[row][col - 1] == startColor)
+            fill(row, col - 1, color, image, startColor);
+        if(row + 1 < image.size()  && image[row + 1][col] == startColor)
+            fill(row + 1, col, color, image, startColor);
+        if(row - 1 >= 0 && image[row - 1][col] == startColor)
+            fill(row - 1, col, color, image, startColor);
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        vector<vector<int>> ans = image;
+        if (color != image[sr][sc]) fill(sr, sc, color, ans, image[sr][sc]);
+        return ans;
+    }
