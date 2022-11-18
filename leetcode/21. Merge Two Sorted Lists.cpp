91f02ffa5fcb38fffe45ae17f012bce0453bbf23
odanchen <91546290+odanchen@users.noreply.github.com>
2022-11-18 05:50:32 -0500

    Create 21. Merge Two Sorted Lists.cpp

---
+++ leetcode/21. Merge Two Sorted Lists.cpp
@@ -0,0 +1,39 @@
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *head;
+        ListNode *cur;
+        
+        if (list1 == NULL && list2 == NULL) return NULL;
+        if (list1 == NULL) return list2;
+        if (list2 == NULL) return list1;
+
+        if(list1->val < list2->val)
+        {
+            cur = new ListNode(list1->val);
+            list1 = list1->next;
+        }
+        else
+        {
+            cur = new ListNode(list2->val);
+            list2 = list2->next;
+        }
+        head = cur;
+
+        while(list1 != nullptr || list2 != nullptr)
+        {
+            ListNode *temp = new ListNode;
+            if (list2 == nullptr || (list1 != nullptr && list1->val < list2->val)) 
+            {
+                temp->val = list1->val;
+                list1 = list1->next;
+            }
+            else
+            {
+                temp->val = list2->val;
+                list2 = list2->next;
+            }
+
+            cur->next = temp;
+            cur = cur->next;
+        }
+        return head;
+    }
