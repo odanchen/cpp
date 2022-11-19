e703c4eeb2f452a211d031d882a9b7f15a8c16aa
odanchen <91546290+odanchen@users.noreply.github.com>
2022-11-19 12:22:14 -0500

    Create 47. Permutations II.cpp

---
+++ leetcode/47. Permutations II.cpp
@@ -0,0 +1,25 @@
+    void addPermutation(int &level, set<vector<int>> &ans, vector<int> arr)
+    {
+        if (level == arr.size() - 1)
+        {
+            ans.insert(arr);
+            return;
+        }
+
+        for(int i = level; i < arr.size(); i++)
+        {
+            swap(arr[level], arr[i]);
+            level++;
+            addPermutation(level, ans, arr);
+            level--;
+            swap(arr[level], arr[i]);
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        set<vector<int>> unique;
+        int level = 0;
+        addPermutation(level, unique, nums);
+        for (vector<int> element : unique) ans.push_back(element);
+        return ans;
+    }   
