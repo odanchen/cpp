f368d2a3a6f8556e44463e0b85503f36fc69e3e4
odanchen <91546290+odanchen@users.noreply.github.com>
2022-11-20 08:04:17 -0500

    Create 198. House Robber.cpp

---
+++ leetcode/198. House Robber.cpp
@@ -0,0 +1,28 @@
+    int rob(vector<int>& nums) {
+        vector<vector<int>> matrix((nums.size() + 4) / 3, vector<int>((nums.size() + 1) / 2 + 1));
+
+        for (int row = 0; row < matrix.size(); row++)
+        {
+            for (int col = 0; col < matrix[row].size(); col++)
+            {
+                int idx = -2 + row * 3 + col * 2;
+                if (idx < 0 || idx >= nums.size()) matrix[row][col] = 0;
+                else matrix[row][col] = nums[idx];
+            }
+        }
+
+        for (int row = 0; row < matrix.size(); row++)
+        {
+            for (int col = 0; col < matrix[row].size(); col++)
+            {
+                int add1, add2;
+                if (row - 1 < 0) add1 = 0;
+                else add1 = matrix[row - 1][col];
+                if (col - 1 < 0) add2 = 0;
+                else add2 = matrix[row][col - 1];
+                matrix[row][col] = max(add1, add2) + matrix[row][col];
+            }
+        }
+
+        return matrix.back().back();
+    }
