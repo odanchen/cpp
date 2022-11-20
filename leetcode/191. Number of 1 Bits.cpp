ba650c9fd1d058900ef3ebbdf7ad5fc45d0681e1
odanchen <91546290+odanchen@users.noreply.github.com>
2022-11-20 10:34:54 -0500

    Create 191. Number of 1 Bits.cpp

---
+++ leetcode/191. Number of 1 Bits.cpp
@@ -0,0 +1,9 @@
+    int hammingWeight(uint32_t n) {
+        int cnt = 0;
+        while (n > 0)
+        {
+            if (n & 1) cnt++;
+            n = n >> 1;
+        }
+        return cnt;
+    }
