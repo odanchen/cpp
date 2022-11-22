a289fa54b3adb6393a82a97e59328e152cdb9f79
odanchen <91546290+odanchen@users.noreply.github.com>
2022-11-22 03:00:21 -0500

    Create 202. Happy Number.cpp

---
+++ leetcode/202. Happy Number.cpp
@@ -0,0 +1,22 @@
+    int nextStep(int n)
+    {
+        int ans = 0;
+        while (n > 0)
+        {
+            ans += ((n % 10) * (n % 10));
+            n /= 10;
+        }
+
+        return ans;
+    }
+    bool isHappy(int n) {
+        set<int> hash;
+
+        while (n != 1 && hash.find(n) == hash.end())
+        {
+            hash.insert(n);
+            n = nextStep(n);
+        }
+
+        return n == 1;
+    }
