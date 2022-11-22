40dbb7666ec98f6774a7dbe7b9b8f81fa05a7167
odanchen <91546290+odanchen@users.noreply.github.com>
2022-11-22 08:02:57 -0500

    Update 733. Flood Fill.cpp

--- leetcode/733. Flood Fill.cpp
+++ leetcode/733. Flood Fill.cpp
@@ -1,15 +1,17 @@
+    bool isInside(int row, int col, vector<vector<int>> &image)
+    {
+        return (row >= 0 && col >= 0 && row < image.size() && col < image[row].size());
+    }
     void fill(int row, int col, int color, vector<vector<int>>& image, int startColor)
     {
-        if (image[row][col] == startColor) image[row][col] = color;
-
-        if(col + 1 < image[row].size()  && image[row][col + 1] == startColor)
+        if (isInside(row, col, image) && image[row][col] == startColor) 
+        {
+            image[row][col] = color;
             fill(row, col + 1, color, image, startColor);
-        if(col - 1 >= 0  && image[row][col - 1] == startColor)
             fill(row, col - 1, color, image, startColor);
-        if(row + 1 < image.size()  && image[row + 1][col] == startColor)
             fill(row + 1, col, color, image, startColor);
-        if(row - 1 >= 0 && image[row - 1][col] == startColor)
             fill(row - 1, col, color, image, startColor);
+        }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         if (color != image[sr][sc]) fill(sr, sc, color, image, image[sr][sc]);
