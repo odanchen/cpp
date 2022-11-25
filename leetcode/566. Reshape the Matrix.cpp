cf386331f3276f94a581c7c4b9849d88a489c4dd
odanchen <91546290+odanchen@users.noreply.github.com>
2022-11-25 05:58:04 -0500

    Create 566. Reshape the Matrix.cpp

---
+++ leetcode/566. Reshape the Matrix.cpp
@@ -0,0 +1,14 @@
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        vector<vector<int>> ans(r, vector<int>(c));
+        if ((r * c) != (mat.size() * mat[0].size())) return mat;
+        int idx = 0;
+        for (vector<int> row : mat)
+        {
+            for (int element : row)
+            {
+                ans[idx / c][idx % c] = element;
+                idx++;
+            }
+        }
+        return ans;
+    }
