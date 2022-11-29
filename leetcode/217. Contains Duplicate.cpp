159bd956c6765e9295e3020178f02ec43eb6d27c
odanchen <91546290+odanchen@users.noreply.github.com>
2022-11-29 02:48:23 -0500

    Create 217. Contains Duplicate.cpp

---
+++ leetcode/217. Contains Duplicate.cpp
@@ -0,0 +1,10 @@
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> mlt;
+        for (int element : nums)
+        {
+            if (mlt.find(element) != mlt.end()) return true;
+            else mlt.insert(element);
+        }
+
+        return false;
+    }
