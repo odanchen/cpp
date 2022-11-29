a161ba38e7e1014c9c0ed6adf2eb162a192ee9f7
odanchen <91546290+odanchen@users.noreply.github.com>
2022-11-29 02:44:58 -0500

    Create 242. Valid Anagram.cpp

---
+++ leetcode/242. Valid Anagram.cpp
@@ -0,0 +1,8 @@
+    bool isAnagram(string s, string t) {
+        vector<int> hash(26);
+        for (char letter : s) hash[letter - 'a']++;
+        for (char letter : t) hash[letter - 'a']--;
+
+        for (int element : hash) if (element != 0) return false;
+        return true;
+    }
