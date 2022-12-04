eae95b62bc71bbecbcf118f5737a671f6d821c3a
odanchen <91546290+odanchen@users.noreply.github.com>
2022-12-04 08:04:10 -0500

    Create 1129. Shortest Path with Alternating Colors.cpp

---
+++ leetcode/1129. Shortest Path with Alternating Colors.cpp
@@ -0,0 +1,75 @@
+    const char red = 'R';
+    const char blue = 'B';
+
+    struct Node
+    {
+        struct Edge
+        {
+            Node* finish;
+            char color;
+            bool visited = false;
+            Edge(Node* _finish, int _color) : finish(_finish), color(_color) {}
+        };
+        int path = -1;
+        vector<Edge*> edges;
+    };
+
+    struct pathNode
+    {
+        Node* node;
+        char color;
+        pathNode(Node* _node, char _color) : node(_node), color(_color) {}
+    };
+
+    void addNodes(pathNode cur, queue<pathNode> &nodeQueue, int &nextTurn)
+    {
+        for (Node::Edge* edge : cur.node->edges)
+        {
+            if (edge->visited == false && edge->color != cur.color)
+            {
+                nextTurn++;
+                edge->visited = true;
+                nodeQueue.push(pathNode(edge->finish, edge->color));
+            }
+        }
+    }
+
+    vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
+        vector<Node*> nodes(n);
+        for (int i = 0; i < nodes.size(); i++) nodes[i] = new Node;
+        for (vector<int> curEdge : redEdges)
+        {
+            nodes[curEdge[0]]->edges.push_back(new Node::Edge(nodes[curEdge[1]], red));
+        }
+        for (vector<int> curEdge : blueEdges)
+        {
+            nodes[curEdge[0]]->edges.push_back(new Node::Edge(nodes[curEdge[1]], blue));
+        }
+        queue<pathNode> nodeQueue;
+        int turn = 0, toVisit = 0, nextTurn = 0;
+        for (Node::Edge* edge : nodes[0]->edges)
+        {
+            nodeQueue.push(pathNode(edge->finish, edge->color));
+            edge->visited = true;
+        }
+        nodes[0]->path = 0;
+        toVisit = (int)nodes[0]->edges.size();
+        
+        while(not(nodeQueue.empty()))
+        {
+            turn++;
+            for (int i = 0; i < toVisit; i++)
+            {
+                pathNode cur = nodeQueue.front();
+                nodeQueue.pop();
+                if (cur.node->path == -1) cur.node->path = turn;
+                addNodes(cur, nodeQueue, nextTurn);
+            }
+            toVisit = nextTurn;
+            nextTurn = 0;
+        }
+        
+        vector<int> ans(n);
+        for (int i = 0; i < n; i++) ans[i] = nodes[i]->path;
+        return ans;
+    }
