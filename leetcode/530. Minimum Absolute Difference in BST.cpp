c74f9cc3e3dfab2b4b4c91e22fd2cf7e0ce32227
odanchen <91546290+odanchen@users.noreply.github.com>
2023-01-24 16:08:12 -0500

    Update 530. Minimum Absolute Difference in BST.cpp

--- leetcode/530. Minimum Absolute Difference in BST.cpp
+++ leetcode/530. Minimum Absolute Difference in BST.cpp
@@ -1,3 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
     void bst(TreeNode *root, vector<int> &nodes)
     {
         if (root == NULL) return;
@@ -18,3 +31,4 @@
 
         return diff;
     }
+};
