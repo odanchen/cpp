ffee7f56517ee90dc6e94840f0374f341b338c0f
odanchen <91546290+odanchen@users.noreply.github.com>
2023-01-24 09:22:48 -0500

    Update 167. Two Sum II - Input Array Is Sorted.cpp

--- leetcode/167. Two Sum II - Input Array Is Sorted.cpp
+++ leetcode/167. Two Sum II - Input Array Is Sorted.cpp
@@ -1,3 +1,5 @@
+class Solution {
+public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         int left, right;
         left = 0;
@@ -11,3 +13,4 @@
 
         return (vector<int>) {left + 1, right + 1};
     }
+};
