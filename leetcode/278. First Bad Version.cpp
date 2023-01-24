426b6c7f5a06eb1fba4182e4aca2eebe46ef1ea5
odanchen <91546290+odanchen@users.noreply.github.com>
2023-01-24 15:57:44 -0500

    Update 278. First Bad Version.cpp

--- leetcode/278. First Bad Version.cpp
+++ leetcode/278. First Bad Version.cpp
@@ -1,3 +1,8 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
     int firstBadVersion(int n) {
         int left = 1, right = n;
         while(left < right)
@@ -9,3 +14,4 @@
 
         return left;
     }
+};
