3286e160f169948cecfb7962722dd2db15e65174
odanchen <91546290+odanchen@users.noreply.github.com>
2023-01-24 16:11:46 -0500

    Update 589. N-ary Tree Preorder Traversal.cpp

--- leetcode/589. N-ary Tree Preorder Traversal.cpp
+++ leetcode/589. N-ary Tree Preorder Traversal.cpp
@@ -1,3 +1,25 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
     void traverse(Node *root, vector<int> &ans)
     {
         if (root == NULL) return;
@@ -10,3 +32,4 @@
         traverse(root, ans);
         return ans;
     }
+};
