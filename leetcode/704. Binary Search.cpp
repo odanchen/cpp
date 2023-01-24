c42f0a01f31e748ab4ad2e341e5c9cb7d9f75361
odanchen <91546290+odanchen@users.noreply.github.com>
2023-01-24 16:14:52 -0500

    Update 704. Binary Search.cpp

--- leetcode/704. Binary Search.cpp
+++ leetcode/704. Binary Search.cpp
@@ -1,3 +1,5 @@
+class Solution {
+public:
     int search(vector<int>& nums, int target) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);
@@ -14,3 +16,4 @@
         if (nums[left] == target) return left;
         return - 1;
     }
+};
