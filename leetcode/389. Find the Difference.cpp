bd76c181e5a9dadd3be80ffa9239606f26e494dc
odanchen <91546290+odanchen@users.noreply.github.com>
2023-01-24 16:02:18 -0500

    Update 389. Find the Difference.cpp

--- leetcode/389. Find the Difference.cpp
+++ leetcode/389. Find the Difference.cpp
@@ -1,6 +1,9 @@
+class Solution {
+public:
     char findTheDifference(string s, string t) {
         int missing = 0;
         for (char letter : s) missing = missing ^ ((int) letter);
         for (char letter : t) missing = missing ^ ((int) letter);
         return (char)missing;
     }
+};
