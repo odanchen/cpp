e8d3b879aad4e545fab2d1064dec866177073740
odanchen <91546290+odanchen@users.noreply.github.com>
2023-01-24 08:15:39 -0500

    Update 1. Two Sum.cpp

--- leetcode/1. Two Sum.cpp
+++ leetcode/1. Two Sum.cpp
@@ -1,3 +1,5 @@
+class Solution {
+public:
     vector<int> twoSum(vector<int>& nums, int target) {
         map<int, int> hashMap;
         for (int i = 0; i < nums.size(); i++)
@@ -15,3 +17,4 @@
 
         return nums;
     }
+};
