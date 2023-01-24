ef6365e65b8a2c3e044f369ed378514eed4e1989
odanchen <91546290+odanchen@users.noreply.github.com>
2023-01-24 16:12:42 -0500

    Update 617. Merge Two Binary Trees.cpp

--- leetcode/617. Merge Two Binary Trees.cpp
+++ leetcode/617. Merge Two Binary Trees.cpp
@@ -1,3 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
         if (root1 == nullptr) return root2;
         if (root2 == nullptr) return root1;
@@ -9,3 +22,4 @@
 
         return root1;
     }
+};
