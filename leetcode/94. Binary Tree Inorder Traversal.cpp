4b9cb55a82ecec29fc61a8980e14149ae52ce7e0
odanchen <91546290+odanchen@users.noreply.github.com>
2023-01-24 09:12:04 -0500

    Update 94. Binary Tree Inorder Traversal.cpp

--- leetcode/94. Binary Tree Inorder Traversal.cpp
+++ leetcode/94. Binary Tree Inorder Traversal.cpp
@@ -1,3 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
     void dfs(TreeNode *root, vector<int> &ans)
     {
         if (root == NULL) return;
@@ -11,3 +24,4 @@
         dfs(root, ans);
         return ans;
     }
+};
