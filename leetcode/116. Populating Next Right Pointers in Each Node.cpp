00a5388ce5fc74152c6b71e62f5da50f935b77df
odanchen <91546290+odanchen@users.noreply.github.com>
2023-01-24 09:15:29 -0500

    Update 116. Populating Next Right Pointers in Each Node.cpp

--- leetcode/116. Populating Next Right Pointers in Each Node.cpp
+++ leetcode/116. Populating Next Right Pointers in Each Node.cpp
@@ -1,3 +1,23 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
     void processNode(Node *root)
     {
         if (root->left == NULL) return;
@@ -22,3 +42,4 @@
         dfs(root);
         return root;
     }
+};
