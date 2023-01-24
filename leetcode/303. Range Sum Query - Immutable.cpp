8227ade257ee52678ca4f257517a635a78fb66b5
odanchen <91546290+odanchen@users.noreply.github.com>
2023-01-24 15:59:16 -0500

    Update 303. Range Sum Query - Immutable.cpp

--- leetcode/303. Range Sum Query - Immutable.cpp
+++ leetcode/303. Range Sum Query - Immutable.cpp
@@ -12,3 +12,9 @@ public:
         return prefix[right + 1] - prefix[left];
     }
 };
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
