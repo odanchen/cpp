5aecd4b89fde3326bf3d3fec8cb8160313176f0f
odanchen <91546290+odanchen@users.noreply.github.com>
2023-01-29 10:29:10 -0500

    Update 17. Letter Combinations of a Phone Number.cpp

--- leetcode/17. Letter Combinations of a Phone Number.cpp
+++ leetcode/17. Letter Combinations of a Phone Number.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
-    typedef vector<string> vString;
     typedef string::iterator sIter;
-    void fillMap(map<char, vector<char>> &letterMap) {
-        letterMap.insert(pair<char, vector<char>>('2', vector<char>{'a', 'b', 'c'}));
-        letterMap.insert(pair<char, vector<char>>('3', vector<char>{'d', 'e', 'f'}));
-        letterMap.insert(pair<char, vector<char>>('4', vector<char>{'g', 'h', 'i'}));
-        letterMap.insert(pair<char, vector<char>>('5', vector<char>{'j', 'k', 'l'}));
-        letterMap.insert(pair<char, vector<char>>('6', vector<char>{'m', 'n', 'o'}));
-        letterMap.insert(pair<char, vector<char>>('7', vector<char>{'p', 'q', 'r', 's'}));
-        letterMap.insert(pair<char, vector<char>>('8', vector<char>{'t', 'u', 'v'}));
-        letterMap.insert(pair<char, vector<char>>('9', vector<char>{'w', 'x', 'y', 'z'}));
-    }
-    void generateCombinations(sIter it, vString &ans, string &digits, string cur, map<char, vector<char>> &letterMap) {
+    map<char, string> letterMap;
+    void generateCombinations(sIter it, vector<string> &ans, string &digits, string cur) {
         if (it == digits.end()) {
             ans.push_back(cur);
             return;
         }
 
         for (char letter : letterMap.at(*it)) {
-            generateCombinations(it + 1, ans, digits, cur + letter, letterMap);
+            generateCombinations(it + 1, ans, digits, cur + letter);
         }
     }
     vector<string> letterCombinations(string digits) {
         if(digits.empty()) return vector<string>();
+        
+        auto insert = [&](char digit, string letters){letterMap.insert(pair<char, string>(digit, letters));};
+        insert('2', "abc"); insert('3', "def");
+        insert('4', "ghi"); insert('5', "jkl");
+        insert('6', "mno"); insert('7', "pqrs");
+        insert('8', "tuv"); insert('9', "wxyz");
 
-        map<char, vector<char>> letterMap;
-        fillMap(letterMap);
         vector<string> ans;
-        generateCombinations(digits.begin(), ans, digits, "", letterMap);
+        generateCombinations(digits.begin(), ans, digits, "");
         return ans;
     }
 };
