2fe872c6d2e9d8aeb0197dd4f8cc742f191bb230
odanchen <91546290+odanchen@users.noreply.github.com>
2023-01-29 19:56:53 -0500

    Update 784. Letter Case Permutation.cpp

--- leetcode/784. Letter Case Permutation.cpp
+++ leetcode/784. Letter Case Permutation.cpp
@@ -1,36 +1,18 @@
-    void swapCase(string &s, int pos)
-    {
-        if(isupper(s[pos])) s[pos] = tolower(s[pos]);
-        else s[pos] = toupper(s[pos]);
-    }
-    void addPermutation(int &level, vector<string> &ans, string s)
-    {
-        if (level == s.size())
-        {
-            ans.push_back(s);
-            return;
+class Solution {
+public:
+    void addPermutation(int idx, vector<string> &ans, string s) {
+        if (idx == s.size()) {
+            ans.push_back(s); return;
         }
-
-        if(isalpha(s[level]))
-        {   
-            level++;
-            addPermutation(level, ans, s);
-            level--;
-            swapCase(s, level);
-            level++;
-            addPermutation(level, ans, s);
-            level--;
+        if(isalpha(s[idx])) {
+            s[idx] = toupper(s[idx]); addPermutation(idx + 1, ans, s);
+            s[idx] = tolower(s[idx]); addPermutation(idx + 1, ans, s);
         }
-        else
-        {
-            level++;
-            addPermutation(level, ans, s);
-            level--;
-        }
+        else addPermutation(idx + 1, ans, s);
     }
     vector<string> letterCasePermutation(string s) {
         vector<string> ans;
-        int level = 0;
-        addPermutation(level, ans, s);
+        addPermutation(0, ans, s);
         return ans;
     }
+};
