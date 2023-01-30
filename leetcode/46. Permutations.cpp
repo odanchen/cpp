6254a4b4b4b410a74b0f6b2d3d34869682252983
odanchen <91546290+odanchen@users.noreply.github.com>
2023-01-29 20:59:44 -0500

    Update 46. Permutations.cpp

--- leetcode/46. Permutations.cpp
+++ leetcode/46. Permutations.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    void addPermutation(int &level, vector<vector<int>> &ans, vector<int> arr)
+    void addPermutation(int level, vector<vector<int>> &ans, vector<int> arr)
     {
-        if (level == arr.size() - 1)
-        {
-            ans.push_back(arr);
-            return;
+        if (level == arr.size() - 1) {
+            ans.push_back(arr); return;
         }
-
         for(int i = level; i < arr.size(); i++)
         {
             swap(arr[level], arr[i]);
-            level++;
-            addPermutation(level, ans, arr);
-            level--;
+            addPermutation(level + 1, ans, arr);
             swap(arr[level], arr[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        int level = 0;
-        addPermutation(level, ans, nums);
+        addPermutation(0, ans, nums);
         return ans;
     }
 };
