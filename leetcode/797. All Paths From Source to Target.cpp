2b07f85173a691460fc09aadca07b3b5c72e6b5f
odanchen <91546290+odanchen@users.noreply.github.com>
2023-02-01 12:01:54 -0500

    Update 797. All Paths From Source to Target.cpp

--- leetcode/797. All Paths From Source to Target.cpp
+++ leetcode/797. All Paths From Source to Target.cpp
@@ -1,45 +1,16 @@
-    struct Node
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void addPermutation(vector<int> curPath, vector<vector<int>> &graph, int node)
     {
-        vector<Node*> neighbors;
-        int num;
-    };
-    void addPermutation(Node *cur, set<Node*> visited, vector<int> curPath, Node *prev, vector<vector<int>> &ans, Node *end)
-    {
-        if (prev != NULL)
-        {
-            visited.insert(prev);
-            curPath.push_back(prev->num);
-        }
-
-        if (cur == end)
-        {
-            curPath.push_back(cur->num);
-            ans.push_back(curPath);
-            return;
-        }
-
-        for (Node *neighbor : cur->neighbors)
-        {
-            addPermutation(neighbor, visited, curPath, cur, ans, end);
+        curPath.push_back(node);
+        if (node == graph.size() - 1) ans.push_back(curPath);
+        for (int next : graph[node]) {
+            addPermutation(curPath, graph, next);
         }
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<Node*> nodes(graph.size());
-        for (int i = 0; i < nodes.size(); i++)
-        {
-            nodes[i] = new Node;
-            nodes[i]->num = i;
-        }
-        for (int i = 0; i < graph.size(); i++)
-        {
-            for (int direction : graph[i])
-            {
-                nodes[i]->neighbors.push_back(nodes[direction]);
-            }
-        }
-
-        vector<vector<int>> ans;
-        set<Node*> visited;
-        addPermutation(nodes[0], visited, vector<int>{}, NULL, ans, nodes.back());
+        addPermutation(vector<int>(), graph, 0);
         return ans;
     }
+};
