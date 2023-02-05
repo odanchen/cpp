6ceaeb07904ac006e08268ce2b513c61834ce941
odanchen <91546290+odanchen@users.noreply.github.com>
2023-02-05 09:02:54 -0500

    Create 268. Missing Number.cpp

---
+++ leetcode/268. Missing Number.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) ans = (ans ^ i) ^ nums[i];
+        return ans ^ nums.size();
+    }
+};
