3dd47d1601423684eb60283ce2e638067c8445e8
odanchen <91546290+odanchen@users.noreply.github.com>
2023-02-26 11:02:46 -0500

    Create 75. Sort Colors.cpp

---
+++ leetcode/75. Sort Colors.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int left = 0, mid = 0, right = nums.size() - 1;
+        while(mid <= right) {
+            if (nums[mid] == 0) swap(nums[left++], nums[mid++]);
+            else if (nums[mid] == 1) mid++;
+            else swap(nums[mid], nums[right--]);
+        }
+    }
+};
