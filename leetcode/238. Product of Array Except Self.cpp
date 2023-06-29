28ef33a8973379263219eabdc01c3a4ec8023a59
odanchen <91546290+odanchen@users.noreply.github.com>
2023-06-29 12:37:13 -0400

    Update 238. Product of Array Except Self.cpp

--- leetcode/238. Product of Array Except Self.cpp
+++ leetcode/238. Product of Array Except Self.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int size = nums.size(), temp = *nums.rbegin();
+        int size = nums.size(), temp = nums.back();
         vector<int> ans(size, 1);
-        
+
         for (int i = 1; i < size; i++) ans[i] = ans[i - 1] * nums[i - 1];
         for (int i = size - 2; i >= 0; i--) {
             ans[i] *= temp;
             temp *= nums[i];
         }
-      
+        
         return ans;
     }
 };
