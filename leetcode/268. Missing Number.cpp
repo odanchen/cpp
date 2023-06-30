f2ba7d0d58d1a377f9f5a6f4f3eb76973be32aee
odanchen <91546290+odanchen@users.noreply.github.com>
2023-06-30 10:16:54 -0400

    Update 268. Missing Number.cpp

--- leetcode/268. Missing Number.cpp
+++ leetcode/268. Missing Number.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int ans = 0;
-        for (int i = 0; i < nums.size(); i++) ans = (ans ^ i) ^ nums[i];
-        return ans ^ nums.size();
+        int ans = nums.size();
+        for (int i = 0; i < nums.size(); i++) ans = ans ^ nums[i] ^ i;
+        return ans;
     }
 };
