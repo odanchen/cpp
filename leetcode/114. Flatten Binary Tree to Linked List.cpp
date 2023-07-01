5cf25df21ad60adb20db76fa001de0cbe0796f19
odanchen <91546290+odanchen@users.noreply.github.com>
2023-06-30 20:33:54 -0400

    Update 114. Flatten Binary Tree to Linked List.cpp

--- leetcode/114. Flatten Binary Tree to Linked List.cpp
+++ leetcode/114. Flatten Binary Tree to Linked List.cpp
@@ -11,19 +11,17 @@
  */
 class Solution {
 public:
-    TreeNode* flattenTree(TreeNode* root) {
-        if (root == NULL || (root->left == NULL and root->right == NULL)) return root;
+    void flatten(TreeNode* root) {
+        if (root == NULL || (root->left == NULL and root->right == NULL)) return;
 
         TreeNode* temp = root->right;
-        root->right = flattenTree(root->left);
+        flatten(root->left);
+        root->right = root->left;
         root->left = NULL;
+
         TreeNode* treeEnd = root;
         while(treeEnd->right != NULL) treeEnd = treeEnd->right;
-        treeEnd->right = flattenTree(temp);
-
-        return root;
-    }
-    void flatten(TreeNode* root) {
-        flattenTree(root);
+        flatten(temp);
+        treeEnd->right = temp;
     }
 };
