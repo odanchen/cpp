999746cc3a24ed7e0865d3547e0f3292a2c1fccb
odanchen <91546290+odanchen@users.noreply.github.com>
2023-07-01 10:18:34 -0400

    Update 88. Merge Sorted Array.cpp

--- leetcode/88. Merge Sorted Array.cpp
+++ leetcode/88. Merge Sorted Array.cpp
@@ -1,36 +1,9 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> ans(m + n);
-        int idx1 = 0, idx2 = 0, idx = 0;
-
-        while(idx < m + n)
-        {
-            if (idx1 >= m)
-            {
-                ans[idx] = nums2[idx2];
-                idx++;
-                idx2++;
-            }
-            else if (idx2 >= n)
-            {
-                ans[idx] = nums1[idx1];
-                idx++;
-                idx1++;
-            }
-            else if (nums1[idx1] < nums2[idx2]) 
-            {
-                ans[idx] = nums1[idx1];
-                idx++;
-                idx1++;
-            }
-            else
-            {
-                ans[idx] = nums2[idx2];
-                idx++;
-                idx2++;
-            }
+        while(n > 0 or m > 0) {
+            if (n == 0 or (m > 0 and (nums1[m - 1] > nums2[n - 1]))) nums1[n + m] = nums1[--m];
+            else nums1[n + m] = nums2[--n];
         }
-        nums1 = ans;
     }
 };
