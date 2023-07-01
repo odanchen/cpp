0c81868630b4ed01824a1db92fd2140bacaa6654
odanchen <91546290+odanchen@users.noreply.github.com>
2023-07-01 12:18:17 -0400

    Update 13. Roman to Integer.cpp

--- leetcode/13. Roman to Integer.cpp
+++ leetcode/13. Roman to Integer.cpp
@@ -1,29 +1,13 @@
 class Solution {
 public:
-    void fillMap(int map[])
-    {
-        map['I'] = 1;
-        map['V'] = 5;
-        map['X'] = 10;
-        map['L'] = 50;
-        map['C'] = 100;
-        map['D'] = 500;
-        map['M'] = 1000;
-    }
     int romanToInt(string s) {
-        int map['X' + 1];
-        fillMap(map);
-        char maxSymbol = 'I';
-
-        int sum = 0;
-        for(int i = s.size() - 1; i >= 0; i--)
-        {
-            if(map[maxSymbol] < map[s[i]]) maxSymbol = s[i];
-
-            if(maxSymbol == s[i]) sum += map[s[i]];
-            else sum -= map[s[i]];
+        unordered_map<char, int> map = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, 
+                    {'C', 100}, {'D', 500}, {'M', 1000}};
+        int maxSymbol = 1, sum = 0;
+        for (auto it = s.rbegin(); it != s.rend(); it++) {
+            if (maxSymbol < map[*it]) maxSymbol = map[*it];
+            sum += map[*it] * (maxSymbol == map[*it] ? 1 : -1);
         }
-
         return sum;
     }
 };
