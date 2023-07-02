a7698df903d910792240d39d1f2d17a276c2b3cc
odanchen <91546290+odanchen@users.noreply.github.com>
2023-07-02 12:27:54 -0400

    Update 12. Integer to Roman.cpp

--- leetcode/12. Integer to Roman.cpp
+++ leetcode/12. Integer to Roman.cpp
@@ -5,13 +5,7 @@ public:
         vector<string> map2 = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX","XC"};
         vector<string> map3 = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC","CM"};
         vector<string> map4 = {"", "M", "MM", "MMM"};
-        string ans;
 
-        ans += map4[(num / 1000)];
-        ans += map3[((num % 1000) / 100)];
-        ans += map2[((num % 100) / 10)];
-        ans += map1[((num % 10))];
-
-        return ans;
+        return map4[(num / 1000)] + map3[((num % 1000) / 100)] + map2[((num % 100) / 10)] + map1[((num % 10))];
     }
 };
