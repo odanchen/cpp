f705ac5c8641ae8c6a44a379a83c11cc455d51ab
odanchen <91546290+odanchen@users.noreply.github.com>
2023-07-02 11:55:48 -0400

    Update 278. First Bad Version.cpp

--- leetcode/278. First Bad Version.cpp
+++ leetcode/278. First Bad Version.cpp
@@ -5,13 +5,11 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         int left = 1, right = n;
-        while(left < right)
-        {
+        while(left < right) {
             int mid = left / 2  + right / 2;
             if (isBadVersion(mid)) right = mid;
             else left = mid + 1;
         }
-
         return left;
     }
 };
