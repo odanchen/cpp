bee89cd9997a7b67ad1531acba361dc180ff8480
odanchen <91546290+odanchen@users.noreply.github.com>
2023-07-02 10:53:22 -0400

    Update 14. Longest Common Prefix.cpp

--- leetcode/14. Longest Common Prefix.cpp
+++ leetcode/14. Longest Common Prefix.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        for(int i = 0; i < strs[0].size(); i++)
-        {
-            for (int j = 0; j < strs.size(); j++)
-            {
-                if(i == strs[j].size() || strs[0][i] != strs[j][i])
-                    return strs[0].substr(0, i);
+        for(int i = 0; i < strs[0].size(); i++){
+            for (int j = 0; j < strs.size(); j++){
+                if(i == strs[j].size() || strs[0][i] != strs[j][i]) return strs[0].substr(0, i);
             }
         }
 
