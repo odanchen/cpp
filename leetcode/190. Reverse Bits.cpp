8c69a02ac4b194b75177a8ab6c01aca4fb5e0298
odanchen <91546290+odanchen@users.noreply.github.com>
2023-07-03 12:12:25 -0400

    Update 190. Reverse Bits.cpp

--- leetcode/190. Reverse Bits.cpp
+++ leetcode/190. Reverse Bits.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        uint32_t ans = 0;
-        for (int i = 0; i < 31; i++) 
-        {
-            ans += n & 1;
+        uint32_t ans = n & 1;
+        for (int i = 0; i < 31; i++) {   
             ans = ans << 1;
             n = n >> 1;
+            ans += n & 1;
         }
-        ans += n & 1;
         return ans;
     }
 };
