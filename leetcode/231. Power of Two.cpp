f82905df660a7eef090b461e31b69a91e53be53c
odanchen <91546290+odanchen@users.noreply.github.com>
2023-07-03 13:08:47 -0400

    Update 231. Power of Two.cpp

--- leetcode/231. Power of Two.cpp
+++ leetcode/231. Power of Two.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        set<int> mlt;
-        for (int element : nums)
-        {
-            if (mlt.find(element) != mlt.end()) return true;
-            else mlt.insert(element);
-        }
-
-        return false;
+    bool isPowerOfTwo(int n) {
+        return n > 0 and !(n & (n - 1));
     }
 };
