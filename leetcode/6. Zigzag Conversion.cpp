612944c28b70c5fcd3cbbcff4c18c6fbd266fee6
odanchen <91546290+odanchen@users.noreply.github.com>
2023-07-03 09:35:56 -0400

    Update 6. Zigzag Conversion.cpp

--- leetcode/6. Zigzag Conversion.cpp
+++ leetcode/6. Zigzag Conversion.cpp
@@ -5,12 +5,10 @@ public:
         string ans(s.size(), '0');
         int idx = 0;
         for (int i = 0; i < numRows; i++) {
-            int j = i;
-            while (j < s.size()) {
+            for (int j = i; j < s.size(); j += (i == 0) ? (numRows - 1) * 2 : i * 2) {
                 ans[idx++] = s[j];
                 j += numRows - 1 - i == 0 ? (numRows - 1) * 2 : (numRows - 1 - i) * 2;
                 if (j < s.size()) ans[idx++] = s[j];
-                j += (i == 0) ? (numRows - 1) * 2 : i * 2;
             }
         }
         return ans;
