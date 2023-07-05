b6a67556dceea29eddf1022b271c2180f046b463
odanchen <91546290+odanchen@users.noreply.github.com>
2023-07-05 18:37:33 -0400

    Update 1025. Divisor Game.cpp

--- leetcode/1025. Divisor Game.cpp
+++ leetcode/1025. Divisor Game.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     bool divisorGame(int n) {
-        vector<bool> winPos(n);
-        for (int i = 0; i < 3 and i < n; i++) winPos[i] = i & 1;
-        for (int i = 3; i < n; i++) {
-            for (int div = 1; div <= (i + 1) / 2; div++) {
-                if ((i + 1) % div == 0 and !winPos[i - div]) {
-                    winPos[i] = true;
-                    break;
-                }
-            }
-        }
-        return winPos.back();
+        return !(n & 1);
     }
 };
