f6e0a6be4275346abba66ce40adab81f0e89bc7b
odanchen <91546290+odanchen@users.noreply.github.com>
2023-07-05 07:59:34 -0400

    Create 1493. Longest Subarray of 1's After Deleting One Element.cpp

---
+++ leetcode/1493. Longest Subarray of 1's After Deleting One Element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int len1 = 0, len2 = 0, ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            while (i < nums.size() and nums[i] == 0) i++;
+            if (len1 == 0) while (i < nums.size() and nums[i++] == 1) len1++;
+            else while (i < nums.size() and nums[i++] == 1) len2++;
+            i--;
+            ans = max (ans, len1 + len2);
+            if (i >= nums.size() - 1 or nums[i + 1] == 0) len1 = len2 = 0;
+            else if (len2 != 0) {
+                swap(len1, len2);
+                len2 = 0;
+            }
+        }
+        return ans == nums.size() ? ans - 1 : ans;
+    }
+};
