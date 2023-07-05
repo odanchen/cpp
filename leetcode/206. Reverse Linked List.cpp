fc0a76d76d91bdf2a544d9eb5dba9f08169f90de
odanchen <91546290+odanchen@users.noreply.github.com>
2023-07-04 20:19:51 -0400

    Update 206. Reverse Linked List.cpp

--- leetcode/206. Reverse Linked List.cpp
+++ leetcode/206. Reverse Linked List.cpp
@@ -11,28 +11,14 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode *ans, *cur;
-        if (head == NULL) return NULL;
-        stack<int> values;
-        while(head != NULL)
-        {
-            values.push(head->val);
-            head = head->next;
-        }
-        while(not(values.empty()))
-        {
-            ListNode *temp = new ListNode(values.top());
-            if (head == NULL)
-            {
-                cur = temp;
-                head = cur;
-            }
-            else
-            {
-                cur->next = temp;
-                cur = cur->next;
-            }
-            values.pop();
+        if (head == NULL) return head;
+        ListNode* temp, *next = head->next;
+        head->next = NULL;
+        while (next) {
+            temp = next->next;
+            next->next = head;
+            head = next;
+            next = temp;
         }
         return head;
     }
