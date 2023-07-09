8d40bc3146f1b1adf6db59d6ff96bf27f4574261
odanchen <91546290+odanchen@users.noreply.github.com>
2023-07-09 08:13:39 -0400

    Update 198. House Robber.cpp

--- leetcode/198. House Robber.cpp
+++ leetcode/198. House Robber.cpp
@@ -1,31 +1,9 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        vector<vector<int>> matrix(((nums.size() + 4) / 3), vector<int>((nums.size() + 1) / 2 + 1));
-
-        for (int row = 0; row < matrix.size(); row++)
-        {
-            for (int col = 0; col < matrix[row].size(); col++)
-            {
-                int idx = -2 + row * 3 + col * 2;
-                if (idx < 0 || idx >= nums.size()) matrix[row][col] = 0;
-                else matrix[row][col] = nums[idx];
-            }
+        for (int i = 0; i < nums.size(); i++) {
+            nums[i] += max(i - 2 < 0 ? 0 : nums[i - 2], i - 3 < 0 ? 0 : nums[i - 3]);
         }
-
-        for (int row = 0; row < matrix.size(); row++)
-        {
-            for (int col = 0; col < matrix[row].size(); col++)
-            {
-                int add1, add2;
-                if (row - 1 < 0) add1 = 0;
-                else add1 = matrix[row - 1][col];
-                if (col - 1 < 0) add2 = 0;
-                else add2 = matrix[row][col - 1];
-                matrix[row][col] = max(add1, add2) + matrix[row][col];
-            }
-        }
-
-        return matrix.back().back();
+        return nums.size() > 1 ? max(nums.back(), nums[nums.size() - 2]) : nums.back();
     }
 };
