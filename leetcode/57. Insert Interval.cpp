837f41bf83b08c143f1115b230bea9da8ab80291
odanchen <91546290+odanchen@users.noreply.github.com>
2023-07-10 08:06:01 -0400

    Create 57. Insert Interval.cpp

---
+++ leetcode/57. Insert Interval.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> ans;
+        int idx = 0;
+        while (idx < intervals.size() and intervals[idx][0] < newInterval[0]) ans.push_back(intervals[idx++]);
+        if (ans.empty() or ans.back()[1] < newInterval[0]) ans.push_back(newInterval);
+        else (*ans.rbegin())[1] = max(ans.back()[1], newInterval[1]);
+        while (idx < intervals.size() and intervals[idx][0] <= ans.back()[1]) 
+            (*ans.rbegin())[1] = max(ans.back()[1], intervals[idx++][1]);
+        while (idx < intervals.size()) ans.push_back(intervals[idx++]);
+        return ans;
+    }
+};
