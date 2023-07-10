e693d93586418b9dd8c15db7ff7c71ad14b1aa28
odanchen <91546290+odanchen@users.noreply.github.com>
2023-07-10 07:28:40 -0400

    Update 392. Is Subsequence.cpp

--- leetcode/392. Is Subsequence.cpp
+++ leetcode/392. Is Subsequence.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        vector<char> checkArr;
-        for (char letter : s) checkArr.push_back(letter);
-
+        vector<char> checkArr(s.begin(), s.end());
         int curIdx = 0;
         for (char letter : t) if (curIdx < s.size() && letter == checkArr[curIdx]) curIdx++;
 
