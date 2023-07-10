5d98374cb5ba72173a270323662d2a069fe736cc
odanchen <91546290+odanchen@users.noreply.github.com>
2023-07-10 08:19:07 -0400

    Update 57. Insert Interval.cpp

--- leetcode/57. Insert Interval.cpp
+++ leetcode/57. Insert Interval.cpp
@@ -5,9 +5,9 @@ public:
         int idx = 0;
         while (idx < intervals.size() and intervals[idx][0] < newInterval[0]) ans.push_back(intervals[idx++]);
         if (ans.empty() or ans.back()[1] < newInterval[0]) ans.push_back(newInterval);
-        else (*ans.rbegin())[1] = max(ans.back()[1], newInterval[1]);
-        while (idx < intervals.size() and intervals[idx][0] <= ans.back()[1]) 
-            (*ans.rbegin())[1] = max(ans.back()[1], intervals[idx++][1]);
+        else ans.rbegin()->at(1) = max(ans.back()[1], newInterval[1]);
+        while (idx < intervals.size() and intervals[idx][0] <= ans.back()[1])
+            ans.rbegin()->at(1) = max(ans.back()[1], intervals[idx++][1]);
         while (idx < intervals.size()) ans.push_back(intervals[idx++]);
         return ans;
     }
