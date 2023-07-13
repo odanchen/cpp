0c3a1fb6f4eee70d3611a0477968a935199f19b0
odanchen <91546290+odanchen@users.noreply.github.com>
2023-07-13 10:54:00 -0400

    Update 236. Lowest Common Ancestor of a Binary Tree.cpp

--- leetcode/236. Lowest Common Ancestor of a Binary Tree.cpp
+++ leetcode/236. Lowest Common Ancestor of a Binary Tree.cpp
@@ -12,9 +12,9 @@ public:
     uint8_t dfs(TreeNode* root, TreeNode* p, TreeNode* q, TreeNode* &ans) {
         if (ans or !root) return 0;
         uint8_t check = (root == p ? 1 : 0) | (root == q ? 2 : 0);
-        if (!(check ^ 3) and !ans) ans = root;
+        if (check == 3 and !ans) ans = root;
         check |= dfs(root->left, p, q, ans) | dfs(root->right, p, q, ans);
-        if (!(check ^ 3) and !ans) ans = root;
+        if (check == 3 and !ans) ans = root;
         return check;
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
