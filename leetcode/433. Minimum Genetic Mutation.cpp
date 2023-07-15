536a9a15d3efc83b6d4e4a76fe81b23346e10de4
odanchen <91546290+odanchen@users.noreply.github.com>
2023-07-15 12:23:20 -0400

    Update 433. Minimum Genetic Mutation.cpp

--- leetcode/433. Minimum Genetic Mutation.cpp
+++ leetcode/433. Minimum Genetic Mutation.cpp
@@ -1,69 +1,51 @@
 class Solution {
 public:
-    struct Node
-    {
+    struct Node {
         string gene;
         vector<Node*> adjacents;
         Node(string _gene) : gene(_gene) {}
     };
-    bool isMutation(string s1, string s2)
-    {
+    bool isMutation(string &s1, string &s2) {
         int cnt = 0;
-        for (int i = 0; i < s1.size(); i++)
-        {
-            if (s1[i] != s2[i]) cnt++;
-        }
+        for (int i = 0; i < s1.size(); i++) if (s1[i] != s2[i]) cnt++;
         return cnt == 1;
     }
-    void addNeighbors(Node* node, queue<Node*> &nodeQueue, set<Node*> &visited, int &nextTurn)
-    {
-        for (Node* neighbor : node->adjacents)
-        {
-            if (visited.find(neighbor) == visited.end())
-            {
+    void addNeighbors(Node* node, queue<Node*> &nodeQueue, set<Node*> &visited) {
+        for (Node* neighbor : node->adjacents) {
+            if (visited.find(neighbor) == visited.end()) {
                 visited.insert(neighbor);
                 nodeQueue.push(neighbor);
-                nextTurn++;
             }
         }
     }
-    int minPath(Node* start, Node* finish)
-    {
-        set<Node*> visited;
+    int minPath(Node* start, Node* finish) {
+        set<Node*> visited({start});
         queue<Node*> nodeQueue;
-        int turn = 0, toVisit = 0, nextTurn = 0;
-        visited.insert(start);
-        addNeighbors(start, nodeQueue, visited, toVisit);
+        int turn = 0, toVisit;
+        addNeighbors(start, nodeQueue, visited);
         
-        while(not(nodeQueue.empty()))
-        {
+        while(not(nodeQueue.empty())) {
             turn++;
-            for (int i = 0; i < toVisit; i++)
-            {
+            toVisit = nodeQueue.size();
+            for (int i = 0; i < toVisit; i++) {
                 Node* cur = nodeQueue.front();
                 nodeQueue.pop();
                 if (cur == finish) return turn;
-                addNeighbors(cur, nodeQueue, visited, nextTurn);
+                addNeighbors(cur, nodeQueue, visited);
             }
-            toVisit = nextTurn;
-            nextTurn = 0;
         }
         
         return -1;
     }
     int minMutation(string start, string end, vector<string>& bank) {
-        map<string, Node*> geneNodes;
-        for (string s : bank) geneNodes.insert(pair<string, Node*>(s, new Node(s)));
-        if (geneNodes.find(start) == geneNodes.end()) geneNodes.insert(pair<string, Node*>(start, new Node(start)));
+        unordered_map<string, Node*> geneNodes;
+        for (string &s : bank) geneNodes[s] = new Node(s);
+        if (geneNodes.find(start) == geneNodes.end()) geneNodes[start] = new Node(start);
 
         if(geneNodes.find(end) == geneNodes.end()) return -1;
-        map<string, Node*>::iterator left, right;
-        for (left = geneNodes.begin(); left != geneNodes.end(); left++)
-        {
-            for (right = left; right != geneNodes.end(); right++)
-            {
-                if (left != right && isMutation(left->second->gene, right->second->gene))
-                {
+        for (auto left = geneNodes.begin(); left != geneNodes.end(); left++) {
+            for (auto right = left; right != geneNodes.end(); right++) {
+                if (left != right && isMutation(left->second->gene, right->second->gene)) {
                     right->second->adjacents.push_back(left->second);
                     left->second->adjacents.push_back(right->second);
                 }
