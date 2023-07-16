cac8e2aadb6cbd61a01e1ecff228b4a19f3da379
odanchen <91546290+odanchen@users.noreply.github.com>
2023-07-16 09:02:12 -0400

    Update 3. Longest Substring Without Repeating Characters.cpp

--- leetcode/3. Longest Substring Without Repeating Characters.cpp
+++ leetcode/3. Longest Substring Without Repeating Characters.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int i = 0, maxLen = 0, curLen = 0;
-        while(i != s.size())
-        {
-            int hash[128][2] = {0};
-            while(i != s.size() && hash[s[i]][0] == 0)
-            {
-                hash[s[i]][0] = 1;
-                hash[s[i]][1] = i;
-                i++;
-                curLen++;
-            }
-
-            maxLen = max(maxLen, curLen);
-            curLen = 0;
-            if(i != s.size()) i = hash[s[i]][1] + 1;
+        vector<int> hash(128);
+        int left = 0, right = 0;
+        while (right < s.size()) {
+            hash[s[right++]]++;
+            if (any_of(hash.begin(), hash.end(), [](int a){return a > 1;})) hash[s[left++]]--;
         }
-
-        return maxLen;
+        return right - left;
     }
 };
