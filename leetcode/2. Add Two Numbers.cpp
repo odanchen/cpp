032622619b4147bfecacf6fe08cac1fff0e9be87
odanchen <91546290+odanchen@users.noreply.github.com>
2023-07-16 09:38:34 -0400

    Update 2. Add Two Numbers.cpp

--- leetcode/2. Add Two Numbers.cpp
+++ leetcode/2. Add Two Numbers.cpp
@@ -11,42 +11,16 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *head = new ListNode();
-        ListNode *cur = head;
-        int temp = 0;
-        while (l1 != NULL || l2 != NULL)
-        {
-            int val1, val2;
-
-            if(l1 == NULL) val1 = 0;
-            else val1 = l1->val;
-
-            if(l2 == NULL) val2 = 0;
-            else val2 = l2->val;
-
-            int val3 = val1 + val2 + temp;
-
-            cur->val = (val3) % 10;
-            temp = (val3) / 10;
-
-            if (l1 != NULL) l1 = l1->next;
-            if (l2 != NULL) l2 = l2->next;
-
-            if (l1 != NULL || l2 != NULL)
-            {
-                ListNode *ptrNext = new ListNode();
-                cur->next = ptrNext;
-                cur = cur->next;
-            }
-            else if (temp == 1)
-            {
-                ListNode *ptrNext = new ListNode();
-                cur->next = ptrNext;
-                cur = cur->next;
-                cur->val = temp;
-            }
+        if (l1 == NULL and l2 == NULL) return NULL;
+        int sum = (l1 != NULL ? l1->val : 0) + (l2 != NULL ? l2->val : 0);
+        ListNode* ans = new ListNode(sum % 10);
+        if ((l1 == NULL or l1->next == NULL) and (l2 == NULL or l2->next == NULL) and sum / 10 > 0) 
+            ans->next = new ListNode(sum / 10);
+        else {
+            if (sum / 10 > 0) l1 != NULL and l1->next != NULL ? 
+                l1->next->val += sum / 10 : l2->next->val += sum / 10;
+            ans->next = addTwoNumbers(l1 == NULL ? l1 : l1->next, l2 == NULL ? l2 : l2->next);
         }
-
-        return head;
+        return ans;
     }
 };
