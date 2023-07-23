5559a3c8ea8dd428395de03f34b1047af44600eb
odanchen <91546290+odanchen@users.noreply.github.com>
2023-07-23 23:30:11 +0200

    Update 560. Subarray Sum Equals K.cpp

--- leetcode/560. Subarray Sum Equals K.cpp
+++ leetcode/560. Subarray Sum Equals K.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        vector<int> preFix(nums.size() + 1);
-        int ans = 0;
+        int ans = 0, cur = 0;
         unordered_map<int, int> intMap = {{0, 1}};
-        for (int i = 1; i < preFix.size(); i++) {
-            preFix[i] = preFix[i - 1] + nums[i - 1];
-            int diff = preFix[i] - k;
+        for (int val : nums) {
+            cur += val;
+            int diff = cur - k;
             if (intMap.find(diff) != intMap.end()) ans += intMap[diff];
-            intMap[preFix[i]]++;
+            intMap[cur]++;
         }
         return ans;
     }
