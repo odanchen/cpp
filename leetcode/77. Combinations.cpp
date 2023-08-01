7526efe7414c94c9f379e4cfa26cc15a4087743d
odanchen <91546290+odanchen@users.noreply.github.com>
2023-08-01 19:39:25 +0200

    Update 77. Combinations.cpp

--- leetcode/77. Combinations.cpp
+++ leetcode/77. Combinations.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    void addCombination(int n, int k, vector<int> arr, vector<vector<int>> &ans, int depth)
-    {
-        if (depth == k) 
-        {
+    void addCombination(int n, int k, vector<int> &arr, vector<vector<int>> &ans) {
+        if (arr.size() == k) {
             ans.push_back(arr);
             return;
         }
 
-        int back;
-        if (depth == 0) back = 0;
-        else back = arr[depth - 1];
-        depth++;
-
-        for(int i = back + 1; i <= n; i++)
-        {
-            arr[depth - 1] = i;
-            addCombination(n, k, arr, ans, depth);
+        int back = arr.empty() ? 0 : arr.back();
+        for (int i = back + 1; i <= n; i++) {
+            arr.push_back(i);
+            addCombination(n, k, arr, ans);
+            arr.pop_back();
         }
     }
+
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> ans;
-        vector<int> arr(k);
-        addCombination(n, k, arr, ans, 0);
+        vector<int> arr;
+        addCombination(n, k, arr, ans);
         return ans;
     }
 };
