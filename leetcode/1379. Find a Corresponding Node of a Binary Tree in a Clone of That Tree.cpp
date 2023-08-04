577a054b1ba973920736d2ab309ca4d85ae81194
odanchen <91546290+odanchen@users.noreply.github.com>
2023-08-04 12:34:36 +0200

    Create 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree.cpp

---
+++ leetcode/1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        if (cloned == nullptr or cloned->val == target->val) return cloned;
+        auto ans = getTargetCopy(original->left, cloned->left, target); 
+        if (ans != nullptr) return ans;
+        return getTargetCopy(original->right, cloned->right, target);
+    }
+};
