349a2145a3bd8400fe5871568879d28d5bcec50f
odanchen <91546290+odanchen@users.noreply.github.com>
2023-08-15 09:04:42 -0400

    Create 538. Convert BST to Greater Tree.cpp

---
+++ leetcode/538. Convert BST to Greater Tree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void traverse(TreeNode* root, int &sum) {
+        if (root == nullptr) return;
+
+        traverse(root->right, sum);
+        root->val = sum = root->val + sum;
+        traverse(root->left, sum);
+    }
+    TreeNode* convertBST(TreeNode* root) {
+        int sum = 0;
+        traverse(root, sum);
+        return root;
+    }
+};
